07143cec45ec07caaa6e423dc727372ad636ee1a
mamallama <reneeq25@gmail.com>
2015-10-24 13:35:46 -0400

    Associations/Validations complete

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,30 @@
 class AnswersController < ApplicationController
+  def index
+    answers = Answer.group(params[:question_id]).count
+    render json: answers
+    # respond_to do |format|
+    #   format.html
+    #   format.json {render json: Voter.all.to_json}
+      # render json: Voter.all.to_json
+    # end
+  end
+
+  def create
+    answer = Answer.new
+    answer.question_id = params[:question_id]
+    answer.user_id = params[:user_id]
+
+    if answer.save
+      render json: answer
+    else
+      render json: answer.errors
+    end
+  end
+
+  def destroy
+    answer = Answer.find_by(auth_token: params[:auth_token])
+    voter.vote.destroy
+    render json: "Sunk the battleship."
+  end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-has_many :answer
-has_many :question
+has_many :answers
+has_many :questions
 has_one :question_vote
 has_one :answer_vote
 
