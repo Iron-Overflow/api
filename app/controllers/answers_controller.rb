e62a48b05644bea1bdf67130948ee41d37293380
mamallama <reneeq25@gmail.com>
2015-10-24 22:25:18 -0400

    Change answer controller to help with user auth

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,37 +1,45 @@
 class AnswersController < ApplicationController
   def index
-    # answers = Answer.group(params[:question_id]).count
+    answers = Answer.group(params[:question_id]).count
     render json: Answer.all
-    # respond_to do |format|
-    #   format.html
-    #   format.json {render json: Voter.all.to_json}
-      # render json: Voter.all.to_json
-    # end
   end
 
   def create
-    answer = Answer.new(body: params[:body], question_id: params[:question_id],
-        user_id: params[:user_id])
-
-    if answer.save
-      render json: answer.to_json
-    else
-      render json: answer.errors
+    answer = Answer.new(question_id: params[:question_id], user_id: params[:user_id],
+        body: params[:body], accepted: params[:accepted])
+    if answer.user.auth_token == params [:auth_token]
+      if answer.save
+        render json: answer
+      else
+        render json: answer.errors
+      end
     end
   end
 
   def show
     render json: Answer.find(params[:id])
-    # if question.access_token == params[:auth_token]
-    #   render json: question
-    # else
-    #   render json: question
-    # end
+  end
+
+  def update
+    answer = Answer.find(params[:id])
+    if answer.user.auth_token == params[:auth_token]
+      if answer.save
+        render json: answer
+      else
+        render json: answer.errors
+      end
+    end
   end
 
   def destroy
-    answer = Answer.find_by(auth_token: params[:auth_token])
-    user.answer.destroy
-    render json: "Answer has been deleted."
+    answer = Answer.find(params[:id])
+    if answer.user.auth_token == params[:auth_token]
+      if answer.destroy
+        render json: "Answer has been deleted"
+      else
+        render json: answer.errors
+      end
+    end
   end
+
 end
