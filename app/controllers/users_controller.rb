a39e32b2a94d57eef66875c816e749c6e72ddffb
Doug Bryant <douglas.e.bryant@gmail.com>
2015-10-25 16:10:38 -0400

    Empty commit in order to push to Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-  end
-
-  def create
     # user = User.new(user_params)
     # user.save ? (render 'show'):(render 'error')
   end
