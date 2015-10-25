d444d68a59fad55a9047f3f2b91f686d1f1ced2c
mamallama <reneeq25@gmail.com>
2015-10-25 14:16:30 -0400

    Authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def generate_token
+    @user.auth_token = SecureRandom.hex
+    @user.save
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      generate_token
+      user.save
+    end
+  end
+
+  def destroy
+    @user.auth_token = nil
+    @user.save
+  end
+end
