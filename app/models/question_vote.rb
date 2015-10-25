6cc69a99416fac8953e06ca453543e6d0b064a6d
mamallama <reneeq25@gmail.com>
2015-10-24 23:54:01 -0400

    Add token to user. Hope it works

--- app/models/question_vote.rb
+++ app/models/question_vote.rb
@@ -1,4 +1,12 @@
 class QuestionVote < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  # think this will work?
+  # def updownvote
+  #   @up_votes = self.votes.where(rating: true).count
+  #   @down_votes = self.votes.where(rating: false).count
+  #   @vote_rating = @up_votes - @down_votes
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
-has_many :answers
-has_many :questions
-has_one :question_vote
-has_one :answer_vote
+  has_many :answers
+  has_many :questions
+  has_one :question_vote
+  has_one :answer_vote
 
-validates :name, :email, :password, presence: true
-validates :email, uniqueness: true
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
+  def token #copy pasta from vote project
+    begin
+      self.token = SecureRandom.hex
+    end while self.class.exists?(token: token)
+  end
 
 end
